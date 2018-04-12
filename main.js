7f711f52baff9710e252c642d3fc36200066699c
Andrew Locke <alocke12992@gmail.com>
2018-04-12 10:17:50 -0600

    Adds avatars and sweet ass logo

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
 var loaded = false;
+var students = [];
+
+function renderStudents() {
+  var list = document.getElementById('students')
+  list.innerHTML = null;
+  students.forEach(function (user) {
+    var li = document.createElement('li');
+    li.innerText = user.name
+    list.append(li);
+  })
+}
+
+function sample() {
+  var index = Math.floor(Math.random() * students.length)
+  var student = students[index];
+  students.splice(index, 1)
+  return student;
+}
+
+function selectStudents() {
+  var left = sample();
+  var right = sample();
+  renderStudents();
+  return [left, right]
+}
+
+function createAvatar(position, student) {
+  return "<h5 id='fighter_" + position + "' data-name='" + student.name + "' data-avatar='" + student.avatar + "' class='center fighter'>" + student.name + "</h5><image class='avatar' src='" + student.avatar + "'/>"
+}
+
+function placeFighters(fighting) {
+  var left = createAvatar('left', fighting[0]);
+  var right = createAvatar('right', fighting[1]);
+  var leftBox = document.getElementById('left');
+  var rightBox = document.getElementById('right');
+  leftBox.innerHTML = left;
+  rightBox.innerHTML = right;
+}
 
 function startFight() {
   loaded = true;
@@ -6,6 +44,8 @@ function startFight() {
   var right = document.getElementById('right');
   left.className = 'left fight-box';
   right.className = 'left fight-box';
+  var fighting = selectStudents();
+  placeFighters(fighting);
 }
 
 function pullStudents() {
@@ -13,9 +53,9 @@ function pullStudents() {
     var xhr = new XMLHttpRequest()
     xhr.onreadystatechange = function () {
       if (xhr.readyState == XMLHttpRequest.DONE) {
-        var users = JSON.parse(xhr.responseText);
+        students = JSON.parse(xhr.responseText);
         var list = document.getElementById('students')
-        users.forEach(function (user) {
+        students.forEach(function (user) {
           var li = document.createElement('li');
           li.innerText = user.name
           list.append(li);
